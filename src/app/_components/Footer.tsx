045912c50dbd83f7c50ab9e2f2c8c9e9fa0f0ef6
DevAlegria <yhonier.alegria@gmail.com>
2025-10-23 09:57:04 -0500

    adding icons to footer

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,14 +1,22 @@
+import { FacebookIcon, InstagramIcon, TiktokIcon } from "@/assets/icons";
 import { iconwhite } from "@/assets/logo";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Footer() {
-  return (<footer className="bg-gray-900 px-2 py-12 flex items-center justify-between relative">
+  return (<footer className="bg-gray-900 px-2 py-12 flex flex-col items-center justify-between relative gap-8 md:flex-row">
     <Image src={iconwhite} width={150} height={80} alt="" />
-    <div className="flex gap-2">
-    <div className="w-8 aspect-square bg-white rounded"></div>
-    <div className="w-8 aspect-square bg-white rounded"></div>
-    <div className="w-8 aspect-square bg-white rounded"></div>
+    <div className="flex gap-8">
+    <Link href="https://www.tiktok.com/@motorepuestos_yara" className="w-8 aspect-square bg-white rounded p-1">
+      <TiktokIcon />
+    </Link>
+    <Link href="https://web.facebook.com/people/Moto-Repuestos-Yara/61554632012488/" className="w-8 aspect-square bg-white rounded">
+      <FacebookIcon />
+    </Link>
+    <Link href="https://www.instagram.com/motorepuestos_yara/" className="w-8 aspect-square bg-white rounded">
+      <InstagramIcon />
+    </Link>
     </div>
-    <span className="absolute bottom-0 left-1 text-gray-400">By yhonier.alegria@gmail.com</span>
+    <span className="absolute bottom-0 right-1 text-gray-400">By yhonier.alegria@gmail.com</span>
   </footer>);
 }  
