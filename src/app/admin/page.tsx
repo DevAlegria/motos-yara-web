ceeff9967d804bca6e709771977e709c9d87955f
DevAlegria <yhonier.alegria@gmail.com>
2025-10-27 09:25:53 -0500

    feat: create admin conection

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,8 @@
+export default function Admin() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-3xl font-bold mb-4">Admin Page</h1>
+      <p>Welcome to the admin dashboard.</p>
+    </div>
+  );
+}
