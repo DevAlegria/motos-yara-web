3ca0540ef906d7400f8b92448c894b10df49852c
DevAlegria <yhonier.alegria@gmail.com>
2025-11-09 17:37:21 -0500

    build(supabase): add dependencies to upload images

---
+++ src/lib/supabaseClient.ts
@@ -0,0 +1,10 @@
+// src/lib/supabaseClient.ts
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+console.log("Supabase URL:", supabaseUrl);
+console.log("Supabase Anon Key:", supabaseAnonKey);
+// 👇 Cliente tipado y seguro
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
