fd31816a8e42081ceb1c78f1359aff53ebb88204
DevAlegria <yhonier.alegria@gmail.com>
2025-11-10 10:32:32 -0500

    feat(navbar): first version of mobile navbar for dasboard

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -16,3 +16,13 @@ export function InstagramIcon() {
 export function FacebookIcon() {
     return <svg fill="#000000" width="100%" height="100%" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" data-name="Layer 1"><path d="M15.12,5.32H17V2.14A26.11,26.11,0,0,0,14.26,2C11.54,2,9.68,3.66,9.68,6.7V9.32H6.61v3.56H9.68V22h3.68V12.88h3.06l.46-3.56H13.36V7.05C13.36,6,13.64,5.32,15.12,5.32Z" /></svg>
 }
+export function BarsIcon() {
+    return <svg className="w-full aspect-square text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="none" viewBox="0 0 24 24">
+        <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M5 7h14M5 12h14M5 17h14" />
+    </svg>
+}
+export function AngleLeftIcon() {
+    return <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m15 19-7-7 7-7" />
+    </svg>;
+}
