44193238394e85f7cc378084f0dd2921e3a31fbd
DevAlegria <yhonier.alegria@gmail.com>
2025-11-13 09:43:47 -0500

    Refactor: angle lef icon

--- src/app/_components/atoms/icons/AngleLeft.tsx
+++ src/app/_components/atoms/icons/AngleLeft.tsx
@@ -1,16 +1,20 @@
+
 import { type SVGProps } from "react";
 
-export function BarsIcon(props: SVGProps<SVGSVGElement>) {
+export function AngleLeftIcon(props: SVGProps<SVGSVGElement>) {
   return <svg
-    viewBox="0 0 24 24"
     fill="none"
+    viewBox="0 0 24 24"
     className="w-6 h-6"
+    aria-hidden="true"
     xmlns="http://www.w3.org/2000/svg"
-    {...props}>
+    {...props}
+  >
     <path
       stroke="currentColor"
       strokeLinecap="round"
+      strokeLinejoin="round"
       strokeWidth="2"
-      d="M5 7h14M5 12h14M5 17h14" />
-  </svg>
+      d="m15 19-7-7 7-7" />
+  </svg>;
 }
--- src/app/_components/atoms/icons/icons.ts
+++ src/app/_components/atoms/icons/icons.ts
@@ -1,7 +1,9 @@
 import { BarsIcon } from "./Bars";
+import { AngleLeftIcon } from "./AngleLeft";
 
 export const icons = {
-  bars: BarsIcon
+  bars: BarsIcon,
+  angleLeft: AngleLeftIcon
 };
 
 export type IconName = keyof typeof icons;
