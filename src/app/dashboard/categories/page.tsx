95c3c1daec04ffe72743f84bb58de5fa701610a1
DevAlegria <yhonier.alegria@gmail.com>
2025-11-22 21:36:09 -0500

    fix: remove unused API prefetching in CategoriesPage component

--- src/app/dashboard/categories/page.tsx
+++ src/app/dashboard/categories/page.tsx
@@ -1,10 +1,8 @@
 import { ListCategories } from "./ListCategories";
 import { NewCategory } from "./NewCategory";
-import { api, HydrateClient } from "@/trpc/server";
+import { HydrateClient } from "@/trpc/server";
 
 export default function CategoriesPage() {
-  void api.category.getAll.prefetch();
-
   return (
     <HydrateClient>
       <main className="p-4 space-y-6">
