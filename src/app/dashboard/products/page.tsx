343e9711370aca0f3da4ccbde4aecb32d2eab4ac
DevAlegria <yhonier.alegria@gmail.com>
2025-11-24 10:45:30 -0500

    feat: update ProductsPage to use CreateProduct and ListProducts components

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,16 +1,11 @@
-import Button from "@/app/_components/atoms/Button";
-import Input from "@/app/_components/atoms/Input";
-import TextArea from "@/app/_components/atoms/TextArea";
+import { CreateProduct } from "./CreateProduct";
+import { ListProducts } from "./ListProducts";
 
 export default function ProductsPage() {
   return (
     <main className="p-4 space-y-6">
-      <div className="shadow-md rounded p-4 flex flex-col gap-4">
-        <Input label="Nombre" placeholder="Nuevo producto" />
-        <TextArea label="Descripcion (opcional)" placeholder="caracteristicas" />
-        <Input label="Precio" placeholder="99 999" type="number"/>
-        <Button>Crear Producto</Button>
-      </div>
+      <CreateProduct />
+      <ListProducts />
     </main>
   );
 }
