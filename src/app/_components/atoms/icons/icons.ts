7ac139ebfb45047707d96ea8c5c62794c7eab1e3
DevAlegria <yhonier.alegria@gmail.com>
2025-12-04 20:51:39 -0500

    feat: add SearchIcon component and include it in icons export

--- src/app/_components/atoms/icons/icons.ts
+++ src/app/_components/atoms/icons/icons.ts
@@ -1,11 +1,14 @@
 import { BarsIcon } from "./Bars";
 import { AngleLeftIcon } from "./AngleLeft";
 import { CloseIcon } from "./Close";
+import { SearchIcon } from "./Search";
+
 
 export const icons = {
   bars: BarsIcon,
   angleLeft: AngleLeftIcon,
-  close: CloseIcon
+  close: CloseIcon,
+  search: SearchIcon
 };
 
 export type IconName = keyof typeof icons;
