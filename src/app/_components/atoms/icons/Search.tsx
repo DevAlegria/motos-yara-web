93f0e15b391e821a1b9d7ed9411a26afdf361ba5
DevAlegria <yhonier.alegria@gmail.com>
2025-12-04 21:02:36 -0500

    feat: update Navbar component to use yaraLogo and add IconButton for search

--- src/app/_components/atoms/icons/Search.tsx
+++ src/app/_components/atoms/icons/Search.tsx
@@ -2,10 +2,7 @@ import { type SVGProps } from "react";
 
 export function SearchIcon(props: SVGProps<SVGSVGElement>) {
   return <svg
-    className="w-6 h-6 text-gray-800 dark:text-white"
     aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
     fill="none"
     viewBox="0 0 24 24"
     {...props}
